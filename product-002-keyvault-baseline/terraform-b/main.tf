a3faf4eb97758e892b7ab10968104ab5f15d50a6
jacek.bodal-l-tp <jesuisdev.net@gmail.com>
2025-12-11 14:23:18 +0100

    1112.2025.1423 002 tf go for no compliant proof

---
+++ product-002-keyvault-baseline/terraform-b/main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.55.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    }
+    subscription_id = var.subscription_id
+  }
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "MyResourceGroup-${var.order_num}"
+  location = "westeurope"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "MyPlan-${var.order_num}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku_name            = "F1"
+  os_type             = "Linux"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "MyWebApp-${var.order_num}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.plan.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  https_only = true #false
+
+  site_config {
+    ftps_state      = "FtpsOnly" #"Disabled"
+    minimum_tls_version = "1.0"
+    always_on = false
+  }
+}
+
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "${var.app_name}${var.APP_ID_NUM}kv"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = false
+  soft_delete_retention_days = 30
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "List"
+    ]
+  }
+
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
+}
